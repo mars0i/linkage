15de51b332075ae813554ed3bc78a1e5e766ae75
mars0i <mabrams@uab.edu>
2016-07-05 12:39:30 -0500

    Made chart params available as fn params

--- src/cljs/re_linkage/core.cljs
+++ src/cljs/re_linkage/core.cljs
@@ -14,41 +14,44 @@
               [cljsjs.d3]
               [cljsjs.nvd3]))
 
+;; NOTE to get this to look like the NVD3 examples on the web, it was
+;; crucial to use nv.d3.css instead of or from
+;; resources/public/css/site.css.
+
 ;; -------------------------
 ;; app code
 
-(def het-coord-seqs
-  (let [xs (range 0.000 0.03001 0.0001)  ; different from in two-locus src
-        ys (map #(two/B-het-ratio %, 0.1, 0.5) xs)]
-    [xs ys]))
+(defn het-rat-coords [max-r s h]
+  (let [xs (range 0.000 (+ max-r 0.00001) 0.0001)  ; different from in two-locus src
+        ys (map #(two/B-het-ratio % s h) xs)]
+    (vec (map #(hash-map :x %1 :y %2) xs ys))))
 
-(defn het-rat-coords [[xs ys]]
-    (vec (map #(hash-map :x %1 :y %2) xs ys)))
-
-(defn make-chart-config [coords]
-  (clj->js
-    [{:values coords 
-      :key "het-rat" 
-      :color "#0000ff" 
-      ;:strokeWidth 1 
-      :area false
-      :fillOpacity -1
-      }]))
+(defn make-chart-config [max-r s h]
+  (let [coords (het-rat-coords max-r s h)]
+    (clj->js
+      [{:values coords 
+        :key "het-rat" 
+        :color "#0000ff" 
+        ;:strokeWidth 1 
+        :area false
+        :fillOpacity -1
+        }])))
 
 ;; name atoms with terminal $
 (def chart-config$
-  (r/atom (make-chart-config (het-rat-coords het-coord-seqs))))
+  (r/atom (make-chart-config 0.03 0.1 0.5)))
 
 (defn setup-chart [svg-id chart-config]
   (let [chart (.lineChart js/nv.models)]
+    ;; configure nvd3 chart:
     (-> chart
         (.height 400)
         (.width 600)
-        ;(.margin {:left 100})
+        ;(.margin {:left 100}) ; what does this do?
         (.useInteractiveGuideline true)
         (.duration 300)
         (.pointSize 1)
-        (.showLegend false) ; turn on for multiple lines on same plot
+        (.showLegend false) ; true is useful if for multiple lines on same plot
         (.showYAxis true)
         (.showXAxis true))
     (-> chart.xAxis
@@ -57,24 +60,17 @@
     (-> chart.yAxis
         (.axisLabel "final/init heteterozygosity at B locus")
         (.tickFormat (fn [d] (pp/cl-format nil "~,2f" d))))
+    ;; add chart to dom using d3:
     (.. js/d3
         (select svg-id)
         (datum chart-config)
-        (call chart))
-    chart))
-
-;; NOTE to get this to look like the NVD3 examples on the web, it was
-;; crucial to use nv.d3.css instead of or from
-;; resources/public/css/site.css.
+        (call chart)))) 
+     ;; in nvd3 examples, we return also chart, but not needed here
 
 (defn home-render []
   [:div [:h3 "Effect of selection on a linked neutral locus (Gillespie 2ed sect 4.2)"]
    [:div {:id "chart-div"}
-    [:svg {:id "chart-svg" :height "400px"}]] ; height will be overridden by NVD3, but we need it here so Reagent knows where to put the next div
-    ;[:svg {:id "chart-svg" :height "400px" :width "500px"}]]
-   [:div {:id "data-div"} 
-    [:text {:style {:font-size "12px"}}
-     (pp/cl-format nil "~{ ~,3f~}" (second het-coord-seqs))]]])
+    [:svg {:id "chart-svg" :height "400px"}]]]) ; height will be overridden by NVD3, but we need it here so Reagent knows where to put the next div
 
 (defn home-did-mount [this]
   (setup-chart "#chart-svg" @chart-config$))
