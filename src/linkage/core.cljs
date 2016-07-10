45adf433faf2e649b20f0a84e27ef9e2d87bb3de
mars0i <mabrams@uab.edu>
2016-07-10 08:36:13 -0500

    Fresh start by incremental modification of 'lein figwheel' tree.
    This branch was created by
    	git checkout --orphan <newbranchname>
    	git rm -rf .
    	/bin/rm -Rf .
    	/bin/rm -Rf .lein-env
    and then I copied files from the separate directory tree
    I'd been experimenting with.  That was created using
    'lein figwheel', and then copying my core.cljs and two-locus.cljs
    and index.html from the master of linkage.  Then I incrementally
    modified both those files and project.clj consulting the master of
    this repo.  (This master has a lot of stuff I didn't need, it appears.
    I had created it with 'lein reagent linkage'.)

---
+++ src/linkage/core.cljs
@@ -0,0 +1,176 @@
+;; Marshall Abrams
+;;
+;; This software is copyright 2016 by Marshall Abrams, and is distributed
+;; under the Gnu General Public License version 3.0 as specified in the
+;; the file LICENSE.
+
+(ns linkage.core
+    (:require [reagent.core :as r :refer [atom]]
+              [reagent.session :as session]
+              [secretary.core :as secretary :include-macros true]
+              [accountant.core :as accountant]
+              [cljs.pprint :as pp]
+              [cljsjs.d3]
+              [cljsjs.nvd3]
+              [linkage.two-locus :as two]))
+
+;; NOTE to get this to look like the NVD3 examples on the web, it was
+;; crucial to use nv.d3.css instead of or from
+;; resources/public/css/site.css.
+
+;; -------------------------
+;; app code
+
+(defn het-rat-coords [max-r s h]
+  "Generate heterozygosity final/initial ratio for recombination rates r
+  from 0 to max-r, using selection coefficient s and heterozygote factor h."
+  (let [xs (range 0.000 (+ max-r 0.00001) 0.0001)  ; different from in two-locus src
+        ys (map #(two/B-het-ratio % s h) xs)]
+    (vec (map #(hash-map :x %1 :y %2) xs ys))))
+
+;; At this stage, I'm not using the capabilities of a ratom, so might
+;; as well just use a regular Clojure atom.
+;; Note: I name atoms with terminal $ .
+(defonce chart-params$ (atom {:max-r 0.02 :s 0.1 :h 0.5}))
+
+(defn update-params! [params$ k v]
+  "Update params$ with value v for key k."
+  (swap! params$ assoc k v))
+
+(defn make-chart-config [chart-params$]
+  "Make NVD3 chart configuration data object."
+  (let [{:keys [max-r s h]} @chart-params$]
+    (clj->js
+      [{:values (het-rat-coords max-r s h)
+        :key "het-rat" 
+        :color "#0000ff" 
+        ;:strokeWidth 1 
+        :area false
+        :fillOpacity -1}])))
+
+;(def chart-config (make-chart-config @chart-params$))
+
+(def chart-svg-id "#chart-svg")
+
+(defn make-chart [svg-id chart-params$]
+  "Create an NVD3 line chart with configuration parameters in @chart-params$
+  and attach it to SVG object with id svg-id."
+  (let [chart (.lineChart js/nv.models)]
+    ;; configure nvd3 chart:
+    (-> chart
+        (.height 400)
+        (.width 600)
+        ;(.margin {:left 100}) ; what does this do?
+        (.useInteractiveGuideline true)
+        (.duration 300)
+        (.pointSize 1)
+        (.showLegend false) ; true is useful if for multiple lines on same plot
+        (.showYAxis true)
+        (.showXAxis true))
+    (-> chart.xAxis
+        (.axisLabel "r/s")
+        (.tickFormat (fn [d] (pp/cl-format nil "~,3f" d))))
+    (-> chart.yAxis
+        (.axisLabel "final/init heteterozygosity at B locus")
+        (.tickFormat (fn [d] (pp/cl-format nil "~,2f" d))))
+    ;; add chart to dom using d3:
+    (.. js/d3
+        (select svg-id)
+        (datum (make-chart-config chart-params$))
+        (call chart)))) 
+     ;; in nvd3 examples, we return also chart, but not needed here
+
+(defn float-input 
+  "Create a text input that accepts numbers.  k is keyword to be used to extract
+  a default value from params$, and to be passed to update-params!.  It will also 
+  be converted to a string an set as the id and name properties of the input 
+  element."
+  [k params$ size label]
+  (let [id (name k)]
+    [:span {:id (str id "-div")}
+     [:text (str "    " label ": ") ]
+     [:input {:id id
+              :name id
+              :type "text"
+              :size size
+              :defaultValue (k @params$)
+              :on-change #(update-params! params$ k (js/parseFloat (-> % .-target .-value)))}]
+     ]))
+
+(defn plot-params-form
+  "Create form to allow changing model parameters and creating a new chart."
+  [svg-id chart-params$]
+  [:form 
+   [float-input :s chart-params$ 5 "selection coeff s"]
+   [float-input :h chart-params$ 5 "heterozygote coeff h"]
+   [float-input :max-r chart-params$ 5 "max recomb prob r"]
+   [:text "  "] ; add space before button
+   [:button {:type "button" :on-click #(make-chart svg-id chart-params$)}
+    "re-plot"]])
+
+(defn head []
+  [:head
+   [:meta {:charset "utf-8"}]
+   [:script {:type "text/javascript" :src "js/compiled/linkage.js"}]])
+
+(defn home-render []
+  "Set up main page (except for chart, which is made elsewhere)."
+  (head)
+  [:div
+   [:h3 "Effect of selection on a linked neutral locus (Gillespie's "
+    [:em "Concise Guide"] " 2ed sect 4.2)"]
+   [:text "Marshall Abrams (© 2016, GPL v.3)"]
+   [:div {:id "chart-div"}
+    [:svg {:id "chart-svg" :height "400px"}] ; height will be overridden by NVD3, but we need it here so Reagent knows where to put the next div
+    [plot-params-form chart-svg-id chart-params$]]])
+
+(defn home-did-mount [this]
+  "Add initial chart to main page."
+  (make-chart chart-svg-id chart-params$))
+
+(defn home-page []
+  (r/create-class {:reagent-render home-render
+                   :component-did-mount home-did-mount}))
+
+(defn current-page []
+  [:div [(session/get :current-page)]])
+
+;; -------------------------
+;; Routes
+
+;; THIS IS WHAT MAKES CALLS THE PAGE FUNCTIONS TO CREATE THE PAGES (?)
+;; NOTE THERE IS CORRESPONDING BUT DIFFERENT CODE IN handler.js
+
+(secretary/defroute "/" []
+  (session/put! :current-page #'home-page))
+
+;(secretary/defroute "/about" []
+;  (session/put! :current-page #'about-page))
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (r/render [current-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (accountant/configure-navigation!
+    {:nav-handler
+     (fn [path]
+       (secretary/dispatch! path))
+     :path-exists?
+     (fn [path]
+       (secretary/locate-route path))})
+  (accountant/dispatch-current!)
+  (mount-root))
+
+(init!)
+
+;; ----------------------------
+
+;; From simple figwheel template, with my additions:
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+(defn on-js-reload []
+)
---
+++ src/linkage/two_locus.cljs
@@ -0,0 +1,125 @@
+;; Marshall Abrams
+;;
+;; This software is copyright 2016 by Marshall Abrams, and is distributed
+;; under the Gnu General Public License version 3.0 as specified in the
+;; the file LICENSE.
+
+;; Clojure implementation of model of two-locus selection
+;; in John Gillespie's _Population Genetics: A Concise Guide_, 2nd ed.,
+;; Johns Hopkins University Press 2004, section 4.2, 
+;; as described in Problem 4.3.  cf. Gillespie's Python solution at
+;; the end of the chapter.
+
+(ns linkage.two-locus)
+
+(defn round2
+  "Round a double to the given precision (number of significant digits)"
+  [precision d]
+  (let [factor (Math/pow 10 precision)]
+    (/ (Math/round (* d factor)) factor)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Gillespie's example of a model with selection with linkage.  A is
+;;  the locus on which selection takes place.  The B locus is neutral.
+
+;; Plotting example (reproduces right half of figure 4.4).
+;; Note left half can't be calculated directly; r/s is never negative.
+;; The left half is just the mirror image of the right half.
+;;
+;; (use '[incanter core charts svg]) ; core for view, charts for xy-plot
+;; (let [ys (range 0.0001 0.04001 0.0001)
+;;       xs (map #(B-het-ratio % 0.1 0.5) ys)
+;;       ch (xy-plot ys xs)] ; make chart object
+;;   (view ch) ; display it in a window
+;;   (save-svg ch "het.svg") ; save it to a file
+;;   xs)
+
+(defn next-gen-hap-freqs
+  "Given recombination probability r; selection parameters h and s with
+  w(A1, A1)=1, w(A1, A2)=1-hs, w(A2, A2)=1-s; and previous-generation 
+  frequencies of x1 through x3 for haplotypes A1B1, A1B2, A2B1, with 
+  the frequency x4 of A2B2 being calculated from the others;
+  returns a (lazy) sequence of first three haplotype frequencies for 
+  the next generation, in the same order as in the parameter list.
+  The fourth haplotype x4 can be calculated as (- 1 x1 x2 x3)."
+  [r s h [x1 x2 x3]]
+  (let [x4 (- 1 x1 x2 x3)
+        p1 (+ x1 x2)  ; freq of A1
+        q1 (- 1 p1)   ; freq of A2
+        w-A1 (- 1 (* q1 h s))          ; mean fitness of A1 or x1 or x2 (wBar1)
+        w-A2 (- 1 (* p1 h s) (* q1 s)) ; mean fitness of A2 or x3 or x4 (wBar3)
+        w-bar (- 1 (* 2 p1 q1 h s) (* q1 q1 s)) ; overall mean fitness
+        ;w-bar (- 1 (* q1 s)) ; overall mean fitness: book code's erroneous def
+        D  (- (* x1 x4) (* x2 x3))]    ; linkage disequilibrium
+    (map (fn [+- hap-freq hap-w]  ; cf. pp. 107,117
+           (/ (+- (* hap-freq hap-w) 
+                  (* r D (- 1 (* h s))))
+              w-bar))
+         [-    +    +]        ; and: -  [no need to calc x4 = (- 1 x1 x2 x3)]
+         [x1   x2   x3]       ; and: x4
+         [w-A1 w-A1 w-A2])))  ; and: w-A2
+
+(defn hap-freqs
+  "Iterates next-gen-hap-freqs to generate a lazy sequence of triplets of 
+  haplotype frequencies x1, x2, x3: Given recombination probability r; 
+  selection parameters h and s with w(A1, A1)=1, w(A1, A2)=1-hs, 
+  w(A2, A2)=1-s; and previous-generation frequencies of x1 through x3 
+  for haplotypes A1B1, A1B2, A2B1, with the frequency x4 of A2B2 being 
+  calculated from the others; returns a lazy sequence of triplets 
+  (sequences) of x1, x2, x3.  The fourth haplotype x4 can be calculated 
+  as (- 1 x1 x2 x3)."
+  [r s h x1 x2 x3]
+  (iterate (partial next-gen-hap-freqs r s h)
+           [x1 x2 x3]))
+
+(defn freqs-at-p1-threshold
+  "Given a sequence of haplotype frequency triples, returns the first
+  triple for which the value of p1 = x1 + x2 is > threshold."
+  [threshold freqs]
+  (first 
+    (drop-while (fn [[x1 x2 _]] (<= (+ x1 x2) threshold))
+                freqs)))
+
+(defn B-het
+  "Given a triplet of haplotypes x1, x2, x3, returns the heterozygosity
+  at the B locus."
+  [[x1 _ x3]]
+  (let [p2 (+ x1 x3)]
+    (* 2 p2 (- 1 p2))))
+
+(defn B-het-ratio-at-p1-threshold
+  [threshold freqs]
+  (let [initial-freqs (first freqs)
+        final-freqs (freqs-at-p1-threshold threshold freqs)]
+    ;(println initial-freqs final-freqs)                 ; DEBUG
+    ;(println (B-het final-freqs) (B-het initial-freqs)) ; DEBUG
+    (/ (B-het final-freqs)
+       (B-het initial-freqs))))
+
+(defn B-het-ratio
+  "Given only a value for r, returns the B locus heterozygoisty of the first 
+  element with p1 > 0.9999 from a lazy sequence of triplets of haplotype 
+  frequencies of x1 through x3 for haplotypes A1B1, A1B2, A2B1, using the 
+  parameters specified by Gillespie's problem 4.3:
+  h = 1/2, s = 0.1, x1 = 1/10000, x2 = 0, x3 = 1 - x1.
+  If additional parameters are provided, they'll replace these values."
+  ([r]
+   (B-het-ratio r 0.1 0.5))
+  ([r s h]
+   (B-het-ratio r s h 0.0001 0 0.4999))
+  ([r s h x1 x2 x3]
+   (let [threshold 0.9999] ; make into a parameter?
+     (B-het-ratio-at-p1-threshold threshold
+                                  (hap-freqs r s h x1 x2 x3)))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; old version of last expression in next-gen-hap-freqs:
+;        r-w14-D (* r w14 D)]       ; a repeated product below
+;      (map #(/ % w-bar)               ; cf. pp. 107,117
+;           [(- (* x1 w-bar-1) r-w14-D)
+;            (+ (* x2 w-bar-1) r-w14-D) 
+;            (+ (* x3 w-bar-3) r-w14-D) 
+;            (- (* x4 w-bar-3) r-w14-D)])))
