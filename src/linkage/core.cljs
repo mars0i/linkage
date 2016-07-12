453351cc262db0fa52aaf2bde0a1f1bca2faba12
mars0i <mabrams@uab.edu>
2016-07-12 11:31:01 -0500

    added link to TwoLocus*.md (broken)

--- src/linkage/core.cljs
+++ src/linkage/core.cljs
@@ -121,11 +121,11 @@
   (head)
   [:div
    [:h3 "Effect of selection on a linked neutral locus" [:br]
-        "(Based on Gillespie's "
-    [:em "Concise Guide"] " 2nd ed., section 4.2.)"]
+    [:a {:href "doc/TwoLocusGillespie42.md"}
+     "cf. Gillespie's " [:em "Concise Guide"] " 2nd ed., section 4.2."]]
    [:text "Marshall Abrams (© 2016, GPL v.3)"]
    [:div {:id "chart-div"}
-    [:svg {:id "chart-svg" :height "400px"}] ; height will be overridden by NVD3, but we need it here so Reagent knows where to put the next div
+    [:svg {:id "chart-svg" :height "400px"}] ; FIXME height will be overridden by NVD3, but we need it here so Reagent knows where to put the next div
     [plot-params-form chart-svg-id chart-params$]]])
 
 (defn home-did-mount [this]
